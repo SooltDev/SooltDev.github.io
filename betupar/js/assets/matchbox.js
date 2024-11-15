44501ec360bf64b0c82e71251121ed185a0617bc
Zsolt <foszto.zsolt@gmail.com>
2024-11-15 20:48:07 +0100

    touch

--- betupar/js/assets/matchbox.js
+++ betupar/js/assets/matchbox.js
@@ -163,8 +163,8 @@ class Matchbox extends EventManager{
         // Remove the handlers of mousemove and mouseup
         
         if (isTouchScreen()){
-            document.addEventListener('touchmove', this.#mouseMove);
-            document.addEventListener('touchend', this.#mouseUp);
+            document.removeEventListener('touchmove', this.#mouseMove);
+            document.removeEventListener('touchend', this.#mouseUp);
         } else {
             document.removeEventListener('mousemove', this.#mouseMove);
             document.removeEventListener('mouseup',this.#mouseUp);
