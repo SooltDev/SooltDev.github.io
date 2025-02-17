c1e818bf284a95934f39ac757f7e8611546b0ba0
Fosztó Zsolt <foszto.zsolt@gmail.com>
2025-02-17 13:17:05 +0100

    szoszolo ver 1.0

--- betupar/main.js
+++ betupar/main.js
@@ -1,3 +1,12 @@
+/**
+ * Name: Szószóló
+ * Version: 1.0
+ * Description: Készségfejlesztő játék, tanulásban akadályoztatott gyerekek számára. 
+ * Mely a betűk elsajátítását hívatott elősegíteni
+ * 
+ * Author: Fosztó Zsolt, Tanay Marcell javaslata, és további ötletei alapján
+ * Published: 2025. február
+ */
 import { gameSetup } from "./js/gamesetup.js";
 import { matchLine } from "./js/game.js";
 
