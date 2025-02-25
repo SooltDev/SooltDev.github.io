1d8317009ea9af3597a6f3b04bfb4fc472b6a522
Fosztó Zsolt <foszto.zsolt@gmail.com>
2025-02-25 18:24:27 +0100

    fullscreen

--- betupar/main.js
+++ betupar/main.js
@@ -7,6 +7,18 @@
  * Author: Fosztó Zsolt, Tanay Marcell javaslata, és további ötletei alapján
  * Published: 2025. február
  */
+
+let elem = document.documentElement;
+
+elem.requestFullscreen({ navigationUI: "show" })
+    .then(() => {})
+    .catch((err) => {
+        alert(
+            `An error occurred while trying to switch into fullscreen mode: ${err.message} (${err.name})`,
+        );
+    });
+
+
 import { gameSetup } from "./js/gamesetup.js";
 import { matchLine } from "./js/game.js";
 
